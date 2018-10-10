e421f9fcadc401d9bd98e713160b803089665673
Catherine Bradley and David Mortkowitz <david@>
2018-10-10 16:23:16 -0700

    add html elements and jquery functions

--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Character, fighter, caster, rogue } from './rpg.js';
+// import { fighter } from './rpg.js';
+
+console.log(fighter);
 
 $(document).ready(function() {
+  console.log(fighter.stats[0]);
+  console.log(caster.name);
+  console.log(rogue.name);
+
+  $("#character-health").text(" " + fighter.stats[0]);
+  $("#character-str").text(" " + fighter.attributes[0]);
+
+  $("#fight-button").click(function(){
+    if(fighter.attributes[0] >= 16) {
+      alert("YOU STRONG YOU WIN!!!!!");
+    } else {
+      alert("you are are weak you lose");
+    }
+  })
+
 
 });
--- src/rpg.js
+++ src/rpg.js
@@ -8,6 +8,16 @@ export function Character(name, role, level, attributes, stats, inventory) {
 }
 
 
+// attributes = [Str, Dex, Agi, Chr, Wis, AC] attributes constant
+// stats = [HP, AP] stats deplete
+
+export var fighter = new Character ("Duff", "Fighter", 1, [16, 10, 12, 17, 5, 17], [20, 2], ["sword", "heal potion"]);
+
+export var caster = new Character ("Balthazar", "Spellcaster", 1, [5, 6, 6, 10, 16, 12], [10, 2], ["potion", "fat book"]);
+
+export var rogue = new Character ("Tarence", "Rogue", 1, [5, 16, 13, 14, 10, 14], [14, 2], ["lockpick", "poison"]);
+
+
 Character.prototype.characterFighter = function () {
   return this.name;
 };
@@ -19,6 +29,18 @@ Character.prototype.characterSpellCaster = function () {
 Character.prototype.characterRogue = function () {
   return this.name;
 };
+
+
+// const fight = () => {
+//
+//   document.getElementById('character-health').innerText = fighter.stats[0];
+//   console.log("fighter.stats[0]");
+//
+// }
+
+
+// }
+
 
 // Character.prototype.characterInventory = function () {
 //   return this.inventory[1];
