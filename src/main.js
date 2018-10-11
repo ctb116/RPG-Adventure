624aa089404ca72b1134c55ceeb1679ecd51b188
Catherine Bradley and David Mortkowitz <catherine@>
2018-10-11 16:13:58 -0700

    add battle encounter

--- src/main.js
+++ src/main.js
@@ -2,36 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Character, fighter, caster, rogue, today } from './rpg.js';
+import { Character, fighter, caster, rogue, today, goblin } from './rpg.js';
 // import { fighter } from './rpg.js';
 
 
 $(document).ready(function() {
 
-  $("#character-health").text(" " + fighter.stats[0]);
-  $("#character-str").text(" " + fighter.attributes[0]);
-
-  // var role;
-  // console.log(role);
-
-  //
-  // $("#fighter").click(function() {
-  // var role = this.id
-  // });
-  //
-  // $("#caster").click(function() {
-  // var role = this.id
-  // });
-
-  // $("#fighter").click(function() {
-  // role = this.id;
-  // console.log(role);
-  // return role;
-  // });
-
-
-document.getElementById("date").innerHTML = today;
-
+  document.getElementById("date").innerHTML = today;
+  var escapeButton = document.getElementById("escape");
+  var attackButton = document.getElementById("fightMain");
+          var diceRoll = Math.floor((Math.random() * 20) + 1);
 
   $(".choice").click(function() {
     if (this.id === 'fighter') {
@@ -48,16 +28,116 @@ document.getElementById("date").innerHTML = today;
     $("#selectChoice").hide();
     $("#graduate").show();
 
+    $("#grad").click(function() {
+      $("#graduate").hide();
+      $("#encounterOne").show();
+      escapeButton.disabled = true;
 
-    $("#fight-button").click(function(){
-      if(role.attributes[0] >= 16) {
-        alert("YOU STRONG YOU WIN!!!!!");
-      } else {
-        alert("you are are weak you lose");
+      $(".encounter").click(function(){
+        if(this.id === 'fight') {
+          $("#fightScene").show();
+          $(".encounter").hide();
+        } else if (this.id === 'talk') {
+          $("#talkScene").show();
+        } else if (this.id === 'escape') {
+          $("#escapeScene").show();
+        }
+      });
+    });
+    $("#goblin-health").text(" " + goblin.stats[0]);
+    $("#character-health").text(" " + role.stats[0]);
+    $("#character-str").text(" " + role.attributes[0]);
+    $("#character-dex").text(" " + role.attributes[1]);
+    $("#character-agi").text(" " + role.attributes[2]);
+    $("#character-chr").text(" " + role.attributes[3]);
+    $("#character-wis").text(" " + role.attributes[4]);
+    $("#character-ac").text(" " + role.attributes[5]);
+
+    function goblinAttack() {
+      $("#gameMessage1").show();
+      $("#gameMessage2").hide();
+      // var attackMes = "The goblin is about to attack";
+      // document.getElementById("gameMessage").innerHTML = attackMes;
+      var goblinAttack = 4;
+      role.stats[0] -= goblinAttack;
+      $("#character-health").text(" " + role.stats[0]);
+      attackButton.disabled = false;
+    }
+
+    $(".attack").click(function() {
+      if(this.id === 'fightMain') {
+        $("#gameMessage2").show();
+        $("#gameMessage1").hide();
+        $("#message").hide();
+        // 1d6
+        // var diceRoll = Math.floor((Math.random() * 20) + 1);
+        console.log(diceRoll);
+
+          if(diceRoll === 20) {
+            var playerAttack = role.attributes[0]
+            console.log("critical attack");
+          } else {
+            var playerAttack = Math.floor(Math.random() * role.attributes[0]-1);
+            console.log("normal attack");
+          }
+
+        goblin.stats[0] -= playerAttack;
+        $("#goblin-health").text(" " + goblin.stats[0]);
+        attackButton.disabled = true;
+        setTimeout(goblinAttack, 1000);
+      }
+      // } else if (this.id === 'fightSpecial') {
+      //
+      // } else if (this.id === 'fightTalk') {
+      //
+      // } else if (this.id === 'fightEscape') {
+      //
+      // }
+
+
+      if(role.stats[0] === 0) {
+        alert("you died. Please be kind enough to reset the page to start over");
+      }
+
+      if(goblin.stats[0] <= 0) {
+        $("#fightScene").hide();
+        $("#gameMessage3").show();
+        role.level = 2;
+        console.log(role.level);
+        var loot = goblin.inventory[0];
+        (role.inventory).push(loot);
+        console.log(role.inventory);
+        document.getElementById("loot").innerHTML = loot;
+      }
+
+      if(role.level === 2) {
+        role.attributes[0] += 2;
+        role.attributes[1] += 2;
+        role.attributes[2] += 2;
+        role.attributes[3] += 2;
+        role.attributes[4] += 2;
+
+        $("#character-str").text(" " + role.attributes[0]);
+        $("#character-dex").text(" " + role.attributes[1]);
+        $("#character-agi").text(" " + role.attributes[2]);
+        $("#character-chr").text(" " + role.attributes[3]);
+        $("#character-wis").text(" " + role.attributes[4]);
       }
     });
-
-
+    //
+    // if(role.stats[0] === 0) {
+    //   alert("you died. Please be kind enough to reset the page to start over");
+    // }
+    //
+    // if(goblin.stats[0] <= 0) {
+    //   $("#fightScene").hide();
+    //   $("#gameMessage3").show();
+    //   role.level = 2;
+    //   console.log(role.level);
+    //   var loot = goblin.inventory;
+    //   (role.inventory).push(loot);
+    //   console.log(role.inventory);
+    // }
 
 
   });
--- src/rpg.js
+++ src/rpg.js
@@ -8,8 +8,6 @@ export function Character(name, role, level, attributes, stats, inventory) {
 }
 
 
-// attributes = [Str, Dex, Agi, Chr, Wis, AC] attributes constant
-// stats = [HP, AP] stats deplete
 
 
 // var dayName = days[date.getDay()];
@@ -23,13 +21,15 @@ export var today = (dayNames[date.getDay()] + ", " + date.getUTCDate() + "th of 
 
 
 
-
+// attributes = [Str, Dex, Agi, Chr, Wis, AC] attributes constant
+// stats = [HP, AP] stats deplete
 export var fighter = new Character ("Duff", "Fighter", 1, [16, 10, 12, 17, 5, 17], [20, 2], ["sword", "heal potion"]);
 
 export var caster = new Character ("Balthazar", "Spellcaster", 1, [5, 6, 6, 10, 16, 12], [10, 2], ["potion", "fat book"]);
 
 export var rogue = new Character ("Tarence", "Rogue", 1, [5, 16, 13, 14, 10, 14], [14, 2], ["lockpick", "poison"]);
 
+export var goblin = new Character ("Goblin Man", "Monster", 1, [5, 16, 13, 14, 10, 14], [14, 2], ["chicken leg", "coin"])
 
 Character.prototype.characterFighter = function () {
   return this.name;
